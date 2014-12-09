dfc05651b943fc148249ddbd215f59c45763b029
paramatma9 <paramatma@resiliencesoft.com>
2014-12-09 18:51:58 +0530

    tadays task
    today i completed offices webservices task and  getting calender on the
    edittext in android

--- gen/com/example/demo/R.java
+++ gen/com/example/demo/R.java
@@ -1081,6 +1081,7 @@ containing a value of this type.
         public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
+        public static final int Edusername=0x7f050050;
         public static final int action_bar=0x7f05001c;
         public static final int action_bar_activity_content=0x7f050015;
         public static final int action_bar_container=0x7f05001b;
@@ -1092,12 +1093,15 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050049;
+        public static final int action_settings=0x7f050053;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
         public static final int btnLoginInsignupPage=0x7f050041;
+        public static final int btnSearch=0x7f05004b;
         public static final int btnSignup=0x7f050040;
+        public static final int btnSubmit=0x7f05004a;
+        public static final int btnTaskAssign=0x7f050052;
         public static final int btnUpdate=0x7f050048;
         public static final int btnlogin=0x7f05003e;
         public static final int checkbox=0x7f05002d;
@@ -1111,8 +1115,14 @@ containing a value of this type.
         public static final int edDesignation=0x7f050046;
         public static final int edEmail=0x7f05003f;
         public static final int edEmpAddress=0x7f050043;
+        public static final int edEmpTask=0x7f050051;
         public static final int edEmpname=0x7f050042;
+        public static final int edFromDate=0x7f05004d;
         public static final int edQualification=0x7f050045;
+        public static final int edTaskAllocateDate=0x7f05004f;
+        public static final int edTaskEndDate=0x7f05004e;
+        public static final int edTodate=0x7f05004c;
+        public static final int editText1=0x7f050049;
         public static final int edit_query=0x7f050030;
         public static final int edpassword=0x7f05003c;
         public static final int edusername=0x7f05003d;
@@ -1209,12 +1219,16 @@ containing a value of this type.
         public static final int activity_login=0x7f030018;
         public static final int activity_main=0x7f030019;
         public static final int activity_profile=0x7f03001a;
-        public static final int support_simple_spinner_dropdown_item=0x7f03001b;
+        public static final int activity_task_message=0x7f03001b;
+        public static final int activity_tomorrow=0x7f03001c;
+        public static final int support_simple_spinner_dropdown_item=0x7f03001d;
     }
     public static final class menu {
         public static final int login=0x7f0c0000;
         public static final int main=0x7f0c0001;
         public static final int profile=0x7f0c0002;
+        public static final int task_message=0x7f0c0003;
+        public static final int tomorrow=0x7f0c0004;
     }
     public static final class string {
         /**  Content description for the action bar "home" affordance. [CHAR LIMIT=NONE] 
@@ -1266,6 +1280,8 @@ containing a value of this type.
         public static final int password=0x7f0a0013;
         public static final int title_activity_login=0x7f0a0010;
         public static final int title_activity_profile=0x7f0a0016;
+        public static final int title_activity_task_message=0x7f0a0018;
+        public static final int title_activity_tomorrow=0x7f0a0017;
         public static final int username=0x7f0a0014;
     }
     public static final class style {
---
+++ src/com/example/demo/TomorrowActivity.java
@@ -0,0 +1,227 @@
+package com.example.demo;
+
+import java.util.Calendar;
+
+import android.support.v7.app.ActionBarActivity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+public class TomorrowActivity extends ActionBarActivity {
+    Button button;
+    EditText fromdate,Enddate,Allocatedate,completiondate;
+    static final int DATE_DIALOG_ID = 0;
+    static final int DATE_DIALOG_ID1 = 1;
+    static final int DATE_DIALOG_ID2 = 2;
+    static final int DATE_DIALOG_ID3 = 3;
+//    private int mYear;
+//    private int mMonth;
+//    private int mDay;
+//
+//    private int mYear1;
+//    private int mMonth1;
+//    private int mDay1;
+    private int mYear,mYear1,mYear2,mYear3;
+    private int mMonth,mMonth1,mMonth2,mMonth3;
+    private int mDay,mDay1,mDay2,mDay3;
+    @Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_tomorrow);
+//		 addListenerOnButtonSearch();
+		fromdate=(EditText)findViewById(R.id.edFromDate);
+		Enddate=(EditText)findViewById(R.id.edTodate);
+		Allocatedate=(EditText)findViewById(R.id.edTaskAllocateDate);
+		completiondate=(EditText)findViewById(R.id.edTaskEndDate);
+		    final Calendar c = Calendar.getInstance();
+		    mYear = c.get(Calendar.YEAR);
+		    mMonth = c.get(Calendar.MONTH);
+		    mDay = c.get(Calendar.DAY_OF_MONTH);
+		    fromdate.setText("From_Date");
+		    Enddate.setText("End_Date");
+		    fromdate.setOnTouchListener(new OnTouchListener() {
+				
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					showDialog(DATE_DIALOG_ID);
+					return false;
+				}
+			});
+
+		    Enddate.setOnTouchListener(new OnTouchListener() {
+				
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					showDialog(DATE_DIALOG_ID1);
+					return false;
+				}
+			});
+		    
+		    Allocatedate.setOnTouchListener(new OnTouchListener() {
+				
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					showDialog(DATE_DIALOG_ID2);
+					return false;
+				}
+			});
+		   
+		    completiondate.setOnTouchListener(new OnTouchListener() {
+				
+				@Override
+				public boolean onTouch(View v, MotionEvent event) {
+					// TODO Auto-generated method stub
+					showDialog(DATE_DIALOG_ID3);
+					return false;
+				}
+			});
+		    
+		    
+
+		}
+
+		@Override
+		protected Dialog onCreateDialog(int id) {
+		    switch (id) {
+		    case DATE_DIALOG_ID:
+		        return new DatePickerDialog(this, mDateSetListener, mYear, mMonth, mDay);
+
+		    case DATE_DIALOG_ID1:
+		        return new DatePickerDialog(this, mDateSetListener1, mYear1, mMonth1,mDay1);
+		        
+	        case DATE_DIALOG_ID2:
+	        return new DatePickerDialog(this, mDateSetListener2, mYear2, mMonth2,mDay2);
+	        case DATE_DIALOG_ID3:
+		        return new DatePickerDialog(this, mDateSetListener3, mYear3, mMonth3,mDay3);
+		    }
+		    return null;
+		}
+
+		// updates the date in the TextView
+
+		private void updateDisplay() {
+			fromdate.setText(new StringBuilder()
+		    // Month is 0 based so add 1
+		            .append(mDay).append("-").append(mMonth + 1).append("-").append(mYear));
+		}
+		private void updateDisplay1() {
+
+			Enddate.setText(new StringBuilder()
+		    // Month is 0 based so add 1
+		            .append(mDay1).append("-").append(mMonth1 + 1).append("-").append(mYear1));
+		}
+		
+		private void updateDisplay2() {
+
+			Enddate.setText(new StringBuilder()
+		    // Month is 0 based so add 1
+		            .append(mDay2).append("-").append(mMonth2 + 1).append("-").append(mYear2));
+		}
+		
+		private void updateDisplay3() {
+
+			Enddate.setText(new StringBuilder()
+		    // Month is 0 based so add 1
+		            .append(mDay3).append("-").append(mMonth3 + 1).append("-").append(mYear3));
+		}
+		
+		// the callback received when the user "sets" the date in the dialog
+		private DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+		    @Override
+		    public void onDateSet(DatePicker view, int year, int monthOfYear,
+		            int dayOfMonth) {
+		        // TODO Auto-generated method stub
+		        mYear = year;
+		        mMonth = monthOfYear;
+		        mDay = dayOfMonth;
+		        updateDisplay();
+		    }
+		};
+
+		private DatePickerDialog.OnDateSetListener mDateSetListener1 = new DatePickerDialog.OnDateSetListener() {
+		    @Override
+		    public void onDateSet(DatePicker view, int year1, int monthOfYear1,
+		            int dayOfMonth1) {
+		        // TODO Auto-generated method stub
+		        mYear1 = year1;
+		        mMonth1 = monthOfYear1;
+		        mDay1 = dayOfMonth1;
+		        updateDisplay1();
+		    }
+		};
+		private DatePickerDialog.OnDateSetListener mDateSetListener2 = new DatePickerDialog.OnDateSetListener() {
+		    @Override
+		    public void onDateSet(DatePicker view, int year2, int monthOfYear2,
+		            int dayOfMonth2) {
+		        // TODO Auto-generated method stub
+		        mYear2 = year2;
+		        mMonth2 = monthOfYear2;
+		        mDay2 = dayOfMonth2;
+		        updateDisplay2();
+		    }
+		};
+		
+		private DatePickerDialog.OnDateSetListener mDateSetListener3 = new DatePickerDialog.OnDateSetListener() {
+		    @Override
+		    public void onDateSet(DatePicker view, int year3, int monthOfYear3,
+		            int dayOfMonth3) {
+		        // TODO Auto-generated method stub
+		        mYear3 = year3;
+		        mMonth3 = monthOfYear3;
+		        mDay = dayOfMonth3;
+		        updateDisplay3();
+		    }
+		};
+		
+		
+		
+	
+//	public void addListenerOnButtonSearch()
+//    {
+//    	button=(Button) findViewById(R.id.btnSearch);
+//    	 button.setOnClickListener(new OnClickListener() {
+//			
+//			@Override
+//			public void onClick(View v) {
+//				// TODO Auto-generated method stub
+//				Intent intent=new Intent(TomorrowActivity.this,TaskMessageActivity.class);
+//				startActivity(intent);
+//			}
+//		});
+//    }
+//			
+		
+
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.tomorrow, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
