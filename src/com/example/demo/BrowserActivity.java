32464aeef02174d837971ccc19be24c523e783e1
paramatma9 <paramatma@resiliencesoft.com>
2014-12-23 19:49:33 +0530

    12/23/2014
    timer

--- src/com/example/demo/BrowserActivity.java
+++ src/com/example/demo/BrowserActivity.java
@@ -1,38 +1,112 @@
 package com.example.demo;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 
+import android.R.string;
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
+import android.webkit.WebView.WebViewTransport;
+import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class BrowserActivity extends ActionBarActivity  {
   Button button;
   HttpClient httpclient;
  	HttpPost httppost;
- 	WebView webview;
+ 	private WebView webview;
  	Context context;
+ 	private static final int START_AFTER_SECONDS = 10;
  	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);	
 		setContentView(R.layout.activity_browser);
+		
+		move_next();
+		
 //		Intent intent = new Intent(Intent.ACTION_VIEW, 	 Uri.parse("http://www.google.com"));
 //		startActivity(intent);
 		
-		webview  =(WebView) findViewById(R.id.Webgoogle);
-		webview.loadUrl("http://google.com");
-		
-//		addListenerOnButtonBrowser();
-	}
+//		webview  =(WebView) findViewById(R.id.Webgoogle);
+//		webview.loadUrl("http://google.com");
+//		TimerTask timerTask = new TimerTask() {
+//            @Override
+//            public void run() {
+//                Toast wait = Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_LONG);
+////                wait.setGravity(Gravity.TOP, 0, 0);
+////                wait.show();
+//            }
+//        };
+//        Timer timer = new Timer();
+//        timer.schedule(timerTask, 2000, 2000);
 
+    }
+		
+	
+	public void move_next()
+	{
+	final Handler handle = new Handler();
+	        Runnable delay = new Runnable() {
+	            public void run() 
+	            {  
+	            	Intent intent = new Intent(Intent.ACTION_VIEW, 	 Uri.parse("http://www.google.com"));
+	           	Toast.makeText(BrowserActivity.this, "hello Siyasut Paramatma Sharan Upadhyay",
+							Toast.LENGTH_SHORT).show();
+	            	BrowserActivity.this.startService(intent);
+	            }
+	        };
+	        handle.postDelayed(delay,START_AFTER_SECONDS*60);
+	}
+	
+		
+//		new Handler().postDelayed(new Runnable() {
+//			
+//			@Override
+//			public void run() {
+//				// TODO Auto-generated method stub
+//				Uri uri= Uri.parse("http://www.google.com");
+//				
+//				final Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+//				startActivity(intent);
+//				BrowserActivity.this.closeContextMenu();
+//			}
+//		},5000);
+//		
+		
+		
+		
+		
+
+
+//           new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                final Intent mainIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+//                BrowserActivity.this.startActivity(mainIntent);
+//                BrowserActivity.this.finish();
+//            }
+//        }, 5000);
+		
+		//addListenerOnButtonBrowser();
+		
+
+
+
+	
 	
 	
 //	public void addListenerOnButtonBrowser()
@@ -53,7 +127,9 @@ public class BrowserActivity extends ActionBarActivity  {
 //		});
 //	}
 	
-	
+
+
+
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
